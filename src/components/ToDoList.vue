e04797b6f2918230d6b7ffa577e50a140012ffce
jisue <tjdwltn121@gmail.com>
2022-09-06 20:12:57 +0900

    미션 적음

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>ToDoList 예제 코드입니다.</h2>
+    <div>추가 input 부분입니다.</div>
+    <div>list 나오는 부분입니다.</div>
+  </div>
+
+</template>
+
+<script>
+/**
+ * 미션~!!
+ * 1. Vuetify를 활용해서 레이아웃을 그린다.
+ * 2. input 박스에 텍스트를 입력시 리스트 배열에 추가 한다.
+ * 3. Delete 버튼을 클릭하면 리스트에서 사라진다
+ * 4. Update 버튼을 클릭시 텍스트가 input 박스로 바뀌고 수정할 수 있다.
+ * 5. 방향키를 누르면 순서를 바꿀 수 있다.
+ * 6. Vuex로 해당 메소드 변수를 전역에서 관리한다.
+ */
+export default {
+  name: "ToDoList"
+}
+</script>
+
+<style scoped>
+
+
+</style>
