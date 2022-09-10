7449c2799fe87872f32d017b35ed8c00d5ffcb4d
jisue <tjdwltn121@gmail.com>
2022-09-10 17:12:37 +0900

    작성하는 부분, 리스트 부분 분리 리스트에서 header body 부분 데이터 구성 후 반복문 처리

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,52 @@
 <template>
   <div class="layout-wrapper">
-    <to-do-list />
+    <h2 class="title">ToDoList 예제 코드입니다.</h2>
+    <to-do-write @register-to-do="registerToDo" />
+    <to-do-list :todoItems="todoItems" />
   </div>
 </template>
 
 <script>
 import ToDoList from "@/components/ToDoList";
+import ToDoWrite from "@/components/ToDoWrite";
 
 export default {
   name: 'App',
   components: {
+    ToDoWrite,
     ToDoList,
-  }
+  },
+  data() {
+    return {
+      todoItems: [
+        {num:1, content:"이상한 변호사 우영우 보기",addDate:"2012-02-02",updateDate:"2012-02-02",updateAt:'N',deleteAt:'N'},
+      ]
+    }
+  },
+  methods: {
+    registerToDo(todoText) {
+      //TODO:날짜 데이터 luxon 이용하기
+      const data = new Date().toLocaleDateString().replace(/\./g, ".");
+      const todo = {
+        num:this.todoItems.length+1,
+        content:todoText,
+        addDate:data,
+        updateDate:data,
+        updateAt:'N',
+        deleteAt:'N'
+      }
+      this.todoItems=[...this.todoItems,todo]
+    }
+  },
 }
 </script>
 
 <style>
 .layout-wrapper{
   text-align: center;
+}
+.title{
+  margin: 50px 0;
+
 }
 </style>
---
+++ src/components/ToDoWrite.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <input
+        placeholder="오늘 해야 될 일 적기"
+        type="text"
+        class="write-todo-input"
+        v-model="todoText"
+    />
+    <button class="add-button" @click="onRegister">할일 추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoWrite",
+  data() {
+    return {
+      todoText: ''
+    }
+  },
+  methods: {
+    onRegister() {
+      this.$emit("register-to-do",this.todoText)
+      this.todoText=''
+    }
+  },
+}
+</script>
+
+<style scoped>
+.write-todo-input {
+  border: 2px solid black;
+  border-radius: 3px;
+  padding: 10px;
+  width: 50%;
+  margin-right: 10px;
+}
+.add-button{
+  border: 2px solid black;
+  border-radius: 3px;
+  padding: 10px;
+  cursor: pointer;
+  width: 10%;
+  background-color: aqua;
+}
+</style>
