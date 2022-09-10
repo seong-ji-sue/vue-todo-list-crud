fc97e12ef83eb55f95b19ae01e927ee3bb9e07be
jisue <tjdwltn121@gmail.com>
2022-09-10 15:19:49 +0900

    레이아웃 구성

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>
-    <h2>ToDoList 예제 코드입니다.</h2>
-    <div>추가 input 부분입니다.</div>
-    <div>list 나오는 부분입니다.</div>
+  <div class="wrapper">
+    <h2 class="title">ToDoList 예제 코드입니다.</h2>
+    <input
+        placeholder="오늘 해야 될 일 적기"
+        type="text"
+        class="write-todo-input"
+        v-model="todoText"
+    />
+    <table class="table common-table-border">
+      <thead>
+        <tr>
+          <th class="common-cell-padding common-table-border">순서</th>
+          <th class="common-cell-padding common-table-border">내용</th>
+          <th class="common-cell-padding common-table-border">추가날짜</th>
+          <th class="common-cell-padding common-table-border">수정날짜</th>
+          <th class="common-cell-padding common-table-border">수정하기</th>
+          <th class="common-cell-padding common-table-border">삭제하기</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="common-cell-padding common-table-border">1</td>
+          <td class="common-cell-padding common-table-border">아이스크림 먹기</td>
+          <td class="common-cell-padding common-table-border">추가날짜</td>
+          <td class="common-cell-padding common-table-border">수정날짜</td>
+          <td class="common-cell-padding common-table-border">수정하기</td>
+          <td class="common-cell-padding common-table-border">삭제하기</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 
 </template>
@@ -18,11 +45,41 @@
  * 6. Vuex로 해당 메소드 변수를 전역에서 관리한다.
  */
 export default {
-  name: "ToDoList"
+  name: "ToDoList",
+  data() {
+    return {
+      todoText: ''
+    }
+  },
 }
 </script>
 
 <style scoped>
+.common-table-border{
+  border: 1px solid black;
+}
+.common-cell-padding{
+  padding: 10px;
+}
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title{
+  margin: 50px 0;
 
+}
+.write-todo-input {
+  border: 2px solid black;
+  border-radius: 3px;
+  padding: 10px;
+  width: 60%;
+}
+.table{
+  margin: 50px;
+  width: 60%;
+  border-spacing: 0px;
+}
 
 </style>
